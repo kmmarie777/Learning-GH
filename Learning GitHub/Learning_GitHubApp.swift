11cb49041dd652871c71a9cb90e4f4ece536c024
katelyn <katelynmello777@gmail.com>
2021-08-31 10:41:56 -0400

    Initial Commit

---
+++ Learning GitHub/Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHubApp.swift
+//  Learning GitHub
+//
+//  Created by Katelyn Mello on 8/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
